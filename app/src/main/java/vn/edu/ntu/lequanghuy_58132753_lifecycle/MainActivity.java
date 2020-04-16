45585201dfa7fc91ecfd450ca95e36f89b6772bd
Quang Huy <lqh6598@gmail.com>
2020-04-16 17:55:59 +0700

    v1.3

--- app/src/main/java/vn/edu/ntu/lequanghuy_58132753_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/lequanghuy_58132753_lifecycle/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class MainActivity extends AppCompatActivity {
+    TextView txtTG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle","onCreate Called");
         Toast.makeText(getApplicationContext(),"onCreate Called",Toast.LENGTH_SHORT).show();
+        txtTG = findViewById(R.id.txtTG);
+        SimpleDateFormat spF = new SimpleDateFormat("HH:mm:ss");
+        String date = spF.format(new Date());
+        txtTG.setText(date);
     }
 
     @Override
